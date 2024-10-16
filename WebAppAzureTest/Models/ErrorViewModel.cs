bd9de940d3c57fddf8e3b342ad47217fff6a72d9
Eliezer Cocom <Eliezer Cocom@HP-VICTUS-ESCC>
2024-10-16 13:55:22 -0600

    Primera actualización de la rama main

---
+++ WebAppAzureTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppAzureTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
